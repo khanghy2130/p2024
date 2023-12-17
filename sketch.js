a1683db5c4477e4d700bf6037273ea8e7c8b0b06
Hy Nguyen <khanghy2130@gmail.com>
2023-12-17 00:50:08 -0600

    test canvas input

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function windowResized(){
 function setup() {
     let mainEle = document.getElementsByTagName("main")[0];
     for (let i=0; i < 100; i++){
-        mainEle.innerHTML += i + "<br>"
+        mainEle.innerHTML += `<button>Button ${i}</button><br>`
     }
 
     createCanvas(
@@ -18,8 +18,11 @@ function setup() {
 
 }
 
+let mx = -999, my = -999;
 let squareCount = 5;
 function draw() {
+    touchCountdown--;
+
     background(40);
     noFill();
     stroke(0);
@@ -29,4 +32,15 @@ function draw() {
             rect(x * width/squareCount, y * height/squareCount, width/squareCount, height/squareCount)
         }
     }
+
+    circle(mx, my, 150);
+}
+
+let touchCountdown = 0;
+function touchEnded(){
+	if (touchCountdown > 0) return;
+	else touchCountdown = 5;
+
+    mx = mouseX;
+    my = mouseY;
 }
