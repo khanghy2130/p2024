60e55c5a86c2a4f47c13121250a3b2ebbe2226fd
Hy Nguyen <khanghy2130@gmail.com>
2024-01-19 03:25:18 -0600

    adjusted hex size & scroll range

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@
     }
 */
 
-const SCALING_FACTOR = 0.15; // controls the hexagon size
+const SCALING_FACTOR = 0.16; // controls the hexagon size
 const BORDER_EXTEND = 1.1; // extends the border limit to include border hexes
 const COLOR_CHANGING_SPEED = 0.015;
 const PARTICLE_SPEED = 0.01;
