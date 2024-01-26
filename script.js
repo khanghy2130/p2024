91631d895a806b3d2f7a612b9b1bae713666ac0d
Hy Nguyen <khanghy2130@gmail.com>
2024-01-26 01:53:28 -0600

    added tool icons

--- script.js
+++ script.js
@@ -1,11 +1,65 @@
 const cc = console.log;
 
+const TOOLS_LIST = [
+    {
+        toolName: "Git", iconSrc: "./tool-icons/Git.png"
+    },
+    {
+        toolName: "HTML", iconSrc: "./tool-icons/HTML.png"
+    },
+    {
+        toolName: "CSS", iconSrc: "./tool-icons/CSS.png"
+    },
+    {
+        toolName: "JavaScript", iconSrc: "./tool-icons/JavaScript.png"
+    },
+    {
+        toolName: "TypeScript", iconSrc: "./tool-icons/TypeScript.png"
+    },
+    {
+        toolName: "React", iconSrc: "./tool-icons/React.png"
+    },
+    {
+        toolName: "Next.js", iconSrc: "./tool-icons/Nextjs.png"
+    },
+    {
+        toolName: "Remix", iconSrc: "./tool-icons/Remix.png"
+    },
+    {
+        toolName: "Tailwind", iconSrc: "./tool-icons/Tailwind.png"
+    },
+    {
+        toolName: "Bootstrap", iconSrc: "./tool-icons/Bootstrap.png"
+    },
+    {
+        toolName: "Sass", iconSrc: "./tool-icons/Sass.png"
+    },
+    {
+        toolName: "p5.js", iconSrc: "./tool-icons/p5js.png"
+    },
+    {
+        toolName: "Socket.io", iconSrc: "./tool-icons/Socketio.png"
+    },
+    {
+        toolName: "Supabase", iconSrc: "./tool-icons/Supabase.png"
+    },
+    {
+        toolName: "Firebase", iconSrc: "./tool-icons/Firebase.png"
+    },
+    {
+        toolName: "MongoDB", iconSrc: "./tool-icons/MongoDB.png"
+    },
+    {
+        toolName: "Docker", iconSrc: "./tool-icons/Docker.png"
+    }
+];
+
+
 let showGreet = true;
 let greetProgress = 0; // 0 to 100
 const tiltPercent = 20; // angle of greet clip path
 const scrollProgressRange = parseInt(getComputedStyle(document.documentElement)
     .getPropertyValue('--scroll-progress-range'));
-
 
 window.addEventListener("load", updateScroll);
 window.addEventListener("scroll", () => {
@@ -18,9 +72,8 @@ window.addEventListener("scroll", () => {
     }
 });
 
-
 let updateScrollTimeout = null;
-function updateScroll(){cc(greetProgress)
+function updateScroll(){
     const oldGreetProgress = greetProgress;
     greetProgress = Math.min(1, window.scrollY / scrollProgressRange) * 100;
 
@@ -51,6 +104,15 @@ function updateScroll(){cc(greetProgress)
     }
 
 }
+
+
+// load tools
+const toolsContainer = document.getElementById("tools-container");
+TOOLS_LIST.forEach(({toolName, iconSrc}) => {
+    const iconElement = new Image();
+    iconElement.src = iconSrc;
+    toolsContainer.appendChild(iconElement);
+})
 
 
 
