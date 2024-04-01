3d4c707ed93f7005e87b9d43ba52aba49ac3efe7
Hy Nguyen <khanghy2130@gmail.com>
2024-04-01 13:05:57 -0500

    updated links

--- js/data.js
+++ js/data.js
@@ -1,139 +1,150 @@
 const TOOLS_LIST = [
-    {
-        toolName: "Git", iconSrc: "./tool-icons/Git.png"
-    },
-    {
-        toolName: "HTML", iconSrc: "./tool-icons/HTML.png"
-    },
-    {
-        toolName: "CSS", iconSrc: "./tool-icons/CSS.png"
-    },
-    {
-        toolName: "JavaScript", iconSrc: "./tool-icons/JavaScript.png"
-    },
-    {
-        toolName: "TypeScript", iconSrc: "./tool-icons/TypeScript.png"
-    },
-    {
-        toolName: "React", iconSrc: "./tool-icons/React.png"
-    },
-    {
-        toolName: "Next.js", iconSrc: "./tool-icons/Nextjs.png"
-    },
-    {
-        toolName: "Remix", iconSrc: "./tool-icons/Remix.png"
-    },
-    {
-        toolName: "Tailwind", iconSrc: "./tool-icons/Tailwind.png"
-    },
-    {
-        toolName: "Bootstrap", iconSrc: "./tool-icons/Bootstrap.png"
-    },
-    {
-        toolName: "Sass", iconSrc: "./tool-icons/Sass.png"
-    },
-    {
-        toolName: "p5.js", iconSrc: "./tool-icons/p5js.png"
-    },
-    {
-        toolName: "Socket.io", iconSrc: "./tool-icons/Socketio.png"
-    },
-    {
-        toolName: "Supabase", iconSrc: "./tool-icons/Supabase.png"
-    },
-    {
-        toolName: "Firebase", iconSrc: "./tool-icons/Firebase.png"
-    },
-    {
-        toolName: "MongoDB", iconSrc: "./tool-icons/MongoDB.png"
-    },
-    {
-        toolName: "Node.js", iconSrc: "./tool-icons/Nodejs.png"
-    }
+  {
+    toolName: "Git",
+    iconSrc: "./tool-icons/Git.png",
+  },
+  {
+    toolName: "HTML",
+    iconSrc: "./tool-icons/HTML.png",
+  },
+  {
+    toolName: "CSS",
+    iconSrc: "./tool-icons/CSS.png",
+  },
+  {
+    toolName: "JavaScript",
+    iconSrc: "./tool-icons/JavaScript.png",
+  },
+  {
+    toolName: "TypeScript",
+    iconSrc: "./tool-icons/TypeScript.png",
+  },
+  {
+    toolName: "React",
+    iconSrc: "./tool-icons/React.png",
+  },
+  {
+    toolName: "Next.js",
+    iconSrc: "./tool-icons/Nextjs.png",
+  },
+  {
+    toolName: "Remix",
+    iconSrc: "./tool-icons/Remix.png",
+  },
+  {
+    toolName: "Tailwind",
+    iconSrc: "./tool-icons/Tailwind.png",
+  },
+  {
+    toolName: "Bootstrap",
+    iconSrc: "./tool-icons/Bootstrap.png",
+  },
+  {
+    toolName: "Sass",
+    iconSrc: "./tool-icons/Sass.png",
+  },
+  {
+    toolName: "p5.js",
+    iconSrc: "./tool-icons/p5js.png",
+  },
+  {
+    toolName: "Socket.io",
+    iconSrc: "./tool-icons/Socketio.png",
+  },
+  {
+    toolName: "Supabase",
+    iconSrc: "./tool-icons/Supabase.png",
+  },
+  {
+    toolName: "Firebase",
+    iconSrc: "./tool-icons/Firebase.png",
+  },
+  {
+    toolName: "MongoDB",
+    iconSrc: "./tool-icons/MongoDB.png",
+  },
+  {
+    toolName: "Node.js",
+    iconSrc: "./tool-icons/Nodejs.png",
+  },
 ];
 
-
 const OTHER_PROJECTS = [
-    {
-        title: "Spread Puzzle",
-        link: "./wait.html?key=Spread_Puzzle",
-        imgSrc: "./project-images/SpreadPuzzle.gif",
-        description: [
-            "Multiplayer puzzle game. Visit ",
-            { text: "GitHub repo", link: "https://github.com/khanghy2130/spread_puzzle"},
-            ". Watch ",
-            { text: "demo video", link: "https://youtu.be/Q57w2F60kAI"},
-            ".",
-        ]
-    },
-    {
-        title: "Course G3C2",
-        link: "https://g3-chapter2.netlify.app",
-        imgSrc: "./project-images/G3C2.gif",
-        description: [
-            "Freelance project. Teach kids to program a rover."
-        ]
-    },
-    {
-        title: "Course G2C8",
-        link: "https://g2-chapter8.netlify.app",
-        imgSrc: "./project-images/G2C8.gif",
-        description: [
-            "Freelance project. Teach kids to program Pong game."
-        ]
-    },
-    {
-        title: "Mystery Treasure",
-        link: "https://mysterytreasure.netlify.app",
-        imgSrc: "./project-images/MysteryTreasure.gif",
-        description: [
-            "Original dueling card game."
-        ]
-    },
-    {
-        title: "Regions",
-        link: "https://editor.p5js.org/LogixIndie/full/S5PqJEaxB",
-        imgSrc: "./project-images/Regions.gif",
-        description: [
-            "Original game that produces generative art."
-        ]
-    },
-    {
-        title: "Shift",
-        link: "https://editor.p5js.org/LogixIndie/full/xwSpzx4Nj",
-        imgSrc: "./project-images/Shift.gif",
-        description: [
-            "Original mind-bending puzzler."
-        ]
-    },
-    {
-        title: "Laser Loop 3",
-        link: "https://editor.p5js.org/LogixIndie/full/Bk4e3ihrt",
-        imgSrc: "./project-images/LaserLoop3.gif",
-        description: [
-            "Original puzzler about laser on cube surface."
-        ]
-    },
-    {
-        title: "Warp",
-        link: "https://editor.p5js.org/LogixIndie/full/HAk3lyV_u",
-        imgSrc: "./project-images/Warp.gif",
-        description: [
-            "Rage game with phenomenal level design."
-        ]
-    },
-    {
-        title: "Tamsac",
-        link: "https://summer-2023.reactjam.com/games/summer-2023-tamsac",
-        imgSrc: "./project-images/Tamsac.gif",
-        description: [
-            "Multiplayer game on ",
-            { text: "Rune", link: "https://www.rune.ai"},
-            ". Made for  ",
-            { text: "React Jam Summer 2023", link: "https://summer-2023.reactjam.com"},
-            ". Watch ",
-            { text: "judges playing", link: "https://www.youtube.com/watch?v=5rvRg5nocD0&t=1484s"},
-            " (24:44 to 29:22)."
-        ]
-    }
+  {
+    title: "Spread Puzzle",
+    link: "./wait.html?key=Spread_Puzzle",
+    imgSrc: "./project-images/SpreadPuzzle.gif",
+    description: [
+      "Multiplayer puzzle game. Visit ",
+      {
+        text: "GitHub repo",
+        link: "https://github.com/khanghy2130/spread_puzzle",
+      },
+      ". Watch ",
+      { text: "demo video", link: "https://youtu.be/Q57w2F60kAI" },
+      ".",
+    ],
+  },
+  {
+    title: "Course G3C2",
+    link: "https://g3-chapter2.netlify.app",
+    imgSrc: "./project-images/G3C2.gif",
+    description: ["Freelance project. Teach kids to program a rover."],
+  },
+  {
+    title: "Course G2C8",
+    link: "https://g2-chapter8.netlify.app",
+    imgSrc: "./project-images/G2C8.gif",
+    description: ["Freelance project. Teach kids to program Pong game."],
+  },
+  {
+    title: "Treasure",
+    link: "https://logix-treasure.netlify.app",
+    imgSrc: "./project-images/MysteryTreasure.gif",
+    description: ["Original dueling card game."],
+  },
+  {
+    title: "Regions",
+    link: "https://logix-regions.netlify.app",
+    imgSrc: "./project-images/Regions.gif",
+    description: ["Original game that produces generative art."],
+  },
+  {
+    title: "Shift",
+    link: "https://logix-shift.netlify.app",
+    imgSrc: "./project-images/Shift.gif",
+    description: ["Original mind-bending puzzler."],
+  },
+  {
+    title: "Laser Loop 3",
+    link: "https://logix-laserloop3.netlify.app",
+    imgSrc: "./project-images/LaserLoop3.gif",
+    description: ["Original puzzler about laser on cube surface."],
+  },
+  {
+    title: "Warp",
+    link: "https://logix-warp.netlify.app",
+    imgSrc: "./project-images/Warp.gif",
+    description: ["Rage game with phenomenal level design."],
+  },
+  {
+    title: "Tamsac",
+    link: "https://summer-2023.reactjam.com/games/summer-2023-tamsac",
+    imgSrc: "./project-images/Tamsac.gif",
+    description: [
+      "Multiplayer game on ",
+      { text: "Rune", link: "https://www.rune.ai" },
+      ". Made for  ",
+      {
+        text: "React Jam Summer 2023",
+        link: "https://summer-2023.reactjam.com",
+      },
+      ". Watch ",
+      {
+        text: "judges playing",
+        link: "https://www.youtube.com/watch?v=5rvRg5nocD0&t=1484s",
+      },
+      " (24:44 to 29:22).",
+    ],
+  },
 ];
